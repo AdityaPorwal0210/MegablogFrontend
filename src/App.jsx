0dab483e5b3d58b8ed1acfbe6e11121a2080a584
Aditya Porwal <adityaporwal3251@gmail.com>
2026-02-06 18:38:13 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,23 @@ import authService from './appwrite/auth'
 function App() {
   const [loading,setLoading]=useState(true)
   const dispatch = useDispatch()
-  useEffect(()=>{ 
+  useEffect(() => {
     authService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      }else{
-        dispatch(logout())
-      }
-    })
-    .finally(()=>setLoading(false))
-  },[])
+        .then((userData) => {
+            if (userData) {
+                dispatch(login({ userData }));
+            } else {
+                dispatch(logout());
+            }
+        })
+        .catch((error) => {
+            // User not logged in - this is expected
+            dispatch(logout());
+        })
+        .finally(() => setLoading(false));
+}, []);
+
+
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
